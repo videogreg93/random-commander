726fc3f9bd9d7a9239ffd52013685c6c36ddf924
Gregory fournier <gregory.fournier@protonmail.com>
2020-11-29 16:52:19 -0500

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/mtg.jpg" class="mtg-logo">
+    <HelloWorld msg="Roll for a commander"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/Form.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.mtg-logo {
+  width: 200px;
+}
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <img :data-src="imageUrl" :src="imageUrl" class="commander-image" />
+    <p>{{ name }}</p>
+    <button v-on:click="rollDeck">Roll</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import Decklist from "../assets/deckList.json";
+
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+  data() {
+    return {
+      name: "",
+      imageUrl: ""
+    };
+  },
+  methods: {
+    rollDeck: function () {
+      const random = Math.floor(Math.random() * this.commanderDecks.length);
+      var deck = this.commanderDecks[random];
+      this.name = this.commanderDecks[random].name;
+      var instance = this;
+      axios
+        .get("https://mtgjson.com/api/v5/decks/" + deck.fileName + ".json")
+        .then((response) => {
+          console.log(response);
+          var imageId = response.data.data.commander[0].identifiers.scryfallId;
+          instance.imageUrl = "https://api.scryfall.com/cards/" + imageId + "?format=image";
+        });
+      console.log(deck);
+    },
+  },
+  mounted() {
+    this.commanderDecks = Decklist["data"].filter((deck) =>
+      deck.type.includes("Commander")
+    );
+
+    console.log(this.commanderDecks);
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.commander-image {
+  width: 300px;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
