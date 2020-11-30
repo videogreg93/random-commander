1f017c145fe348d0fd062e22f8a1e0e25cf5ff79
Gregory Fournier <gfournier@hopper.com>
2020-11-30 13:19:52 -0500

    Can copy decklist now

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -4,6 +4,7 @@
     <img :data-src="imageUrl" :src="imageUrl" class="commander-image" />
     <h3>Deck Name : {{ name }}</h3>
     <button v-on:click="rollDeck">Roll</button>
+    <button v-if="name" v-clipboard="copyDecklist">Copy decklist to clipboard</button>
     <div class="cards-container">
       <div v-for="card in cards" :key="card">
       <img :data-src=card :src=card class="card" />
@@ -23,9 +24,10 @@ export default {
   },
   data() {
     return {
-      name: "",
-      imageUrl: "",
-      cards: []
+      name: null, // Commander deck name
+      imageUrl: "", // Commander image url
+      cards: [], //Card Images
+      decklist: null // Cardlist for importing
     };
   },
   methods: {
@@ -38,11 +40,20 @@ export default {
         .get("https://mtgjson.com/api/v5/decks/" + deck.fileName + ".json")
         .then((response) => {
           console.log(response);
-          var imageId = response.data.data.commander[0].identifiers.scryfallId;
+          var commander = response.data.data.commander[0];
+          var imageId = commander.identifiers.scryfallId;
           instance.cards = response.data.data.mainBoard.map(item => "https://api.scryfall.com/cards/" + item.identifiers.scryfallId + "?format=image")
           instance.imageUrl = "https://api.scryfall.com/cards/" + imageId + "?format=image";
+          instance.decklist = "1 " + commander.name + "\n";
+          response.data.data.mainBoard.forEach((item) => {
+            instance.decklist += item.count + " " + item.name + "\n";
+          });
+          //instance.decklist = response.data.data.mainBoard.map(item => item.count + " " + item.name);
         });
     },
+    copyDecklist: function () {
+      return this.decklist;
+    }
   },
   mounted() {
     this.commanderDecks = Decklist["data"].filter((deck) =>
@@ -65,6 +76,7 @@ export default {
 }
 .card {
   width: 200px;
+  margin: 10px;
 }
 h3 {
   margin: 40px 0 0;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Clipboard from 'v-clipboard'
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
+
+Vue.use(Clipboard);
